ef844ed17b48ef61f3a510e00dfd8ecc899deae2
bjorn.norrliden <bjorn.norrliden@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2008-11-27 22:59:58 +0000

    Tesla.GFX init

---
+++ Tesla.GFX/Texture.cs
@@ -0,0 +1,126 @@
+// Texture.cs created with MonoDevelop
+// User: topfs at 12:37 PM 9/28/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using System.Drawing;
+using Tao.OpenGl;
+using System.Collections;
+using Tesla.Common;
+
+namespace Tesla.GFX
+{	
+	public enum TextureFilter
+	{
+		Nearest,
+		Linear,
+		MipMap
+	}
+
+	public class Texture
+	{
+		private static System.Collections.Generic.Dictionary<int, Texture> LoadedTextures = new System.Collections.Generic.Dictionary<int,Texture>();
+		private int TextureIndex;
+		
+		private int ImageWidth;
+		private int ImageHeight;
+		
+		private Texture(Bitmap image, TextureFilter textureFilter, int wrapMode)
+		{
+			image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+			System.Drawing.Imaging.BitmapData bitmapdata;
+			Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+			
+
+			int []texture = new int[1];
+			Gl.glGenTextures(1, texture);
+
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture[0]);
+			Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, wrapMode);
+			Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, wrapMode);
+			// Create Linear Filtered Texture
+
+			if (textureFilter == TextureFilter.Nearest)
+			{
+			bitmapdata = image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_NEAREST);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_NEAREST);
+				Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, (int)Gl.GL_RGBA, image.Width, image.Height, 0, Gl.GL_BGR_EXT, Gl.GL_UNSIGNED_BYTE, bitmapdata.Scan0);
+			image.UnlockBits(bitmapdata);				
+			}
+			else if (textureFilter == TextureFilter.Linear)
+			{
+				byte []alpha = new byte[image.Width * image.Height];
+				int i = 0;
+				for (int x = 0; x < image.Width; x++)
+				{
+					for (int y = 0; y < image.Height; y++)
+					{
+						Console.Out.WriteLine(x + ", " + y);
+						alpha[i] = image.GetPixel(x, y).A;
+						i++;
+					}
+				}
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+				Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, (int)Gl.GL_ALPHA, image.Width, image.Height, 0, Gl.GL_ALPHA, Gl.GL_UNSIGNED_BYTE, alpha);
+			}
+			else if (textureFilter == TextureFilter.MipMap)
+			{
+				byte []rgba = new byte[image.Width * image.Height * 4];
+				int i = 0;
+				for (int x = 0; x < image.Width; x++)
+				{
+					for (int y = 0; y < image.Height; y++)
+					{
+						//Console.Out.WriteLine(x + ", " + y);
+						rgba[i+0] = image.GetPixel(x, y).R;
+						rgba[i+1] = image.GetPixel(x, y).G;
+						rgba[i+2] = image.GetPixel(x, y).B;
+						rgba[i+3] = 255;//image.GetPixel(x, y).A;
+						i+=4;
+					}
+				}
+			
+			//bitmapdata = image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR_MIPMAP_NEAREST);
+				Glu.gluBuild2DMipmaps(Gl.GL_TEXTURE_2D, (int)Gl.GL_RGBA, image.Width, image.Height, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, rgba);
+				//			image.UnlockBits(bitmapdata);
+			}
+
+			image.Dispose();
+			this.TextureIndex = texture[0];
+		}
+		
+		private Texture(string TexturePath)
+		{
+			throw new NotImplementedException();
+		}
+		public static Texture CreateFromFile(string Path)
+		{
+			return CreateFromFile(Path, TextureFilter.MipMap, Gl.GL_REPEAT);
+		}
+		public static Texture CreateFromFile(string Path, TextureFilter textureFilter, int wrapMode)
+		{
+			Texture t = null;
+			if (!LoadedTextures.TryGetValue(Path.GetHashCode(), out t))
+			{
+				t = new Texture(new Bitmap(Path), textureFilter, wrapMode);
+				LoadedTextures.Add(Path.GetHashCode(), t); 
+			}
+			
+			return t;
+		}
+		
+		public void Bind()
+		{
+			Gl.glEnable(Gl.GL_TEXTURE_2D);
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, TextureIndex);
+			Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+		}
+	}
+}
