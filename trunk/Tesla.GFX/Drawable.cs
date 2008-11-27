1afa49a9e7c70c93e5fb6c9b6c62ae15899dd49a
bjorn.norrliden <bjorn.norrliden@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2008-11-27 22:58:44 +0000

    Tesla.GFX init

---
+++ trunk/Tesla.GFX/Drawable.cs
@@ -0,0 +1,27 @@
+// Drawable.cs created with MonoDevelop
+// User: topfs at 11:44 PM 9/27/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace Tesla.GFX
+{
+	
+	
+	public interface Drawable
+	{
+		void Draw(float frameTime, Frustum frustum);
+	}
+	
+	public abstract class DrawableAnimation : Drawable
+	{
+		void Drawable.Draw(float frameTime, Frustum frustum)
+		{
+			Draw(true);
+		}
+		
+		public abstract void Draw(bool Animate);
+	}
+}
