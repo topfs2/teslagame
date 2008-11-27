82262126641ca2669c46455289365292412676d6
bjorn.norrliden <bjorn.norrliden@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2008-11-27 22:58:11 +0000

    Tesla.GFX init

---
+++ Tesla.GFX/ModelLoading/Face.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tesla.Common;
+using Tao.OpenGl;
+
+namespace Tesla.GFX.ModelLoading
+{
+    public class Face
+    {
+        private Point3f[] vertices;
+        private Point2f[] textureVertices;
+        private Point3f[] normalVertices;
+        private int polygonType;
+
+        public Face(Point3f[] vertices, Point2f[] textureVertices, Point3f[] normalVertices, int polygonType)
+        {
+            this.vertices = vertices;
+            this.textureVertices = textureVertices;
+            this.normalVertices = normalVertices;
+            this.polygonType = polygonType;
+        }
+
+        public int PolygonType()
+        {
+            return polygonType;
+        }
+
+        public void Draw()
+        {
+            if (polygonType != LoadableModel.currentPolygon)
+            {
+                Gl.glEnd();
+                LoadableModel.Init(polygonType);
+                LoadableModel.currentPolygon = polygonType;
+            }
+
+            for(int i = 0; i < vertices.Length; i++)
+            {
+                if (normalVertices.Length > 0)
+                    Gl.glNormal3f(normalVertices[i].x, normalVertices[i].y, normalVertices[i].z);
+                if (textureVertices.Length > 0)
+                    Gl.glTexCoord2f(textureVertices[i].x, textureVertices[i].y);
+                if (vertices.Length > 0)
+                    Gl.glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
+            }
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new StringBuilder();
+            sb.Append("Face:\n");
+            sb.Append(" Vertices: ");
+            foreach (Point3f p in vertices)
+            {
+                sb.Append("[" + p + "]");
+            }
+            sb.Append(" Texture Vertices: ");
+            foreach (Point2f t in textureVertices)
+            {
+                sb.Append("[" + t + "]");
+            }
+            sb.Append(" Normal Vertices: ");
+            foreach (Point3f n in normalVertices)
+            {
+                sb.Append("[" + n + "]");
+            }
+            sb.Append("End of Face");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tesla.GFX/ModelLoading/Material.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Tao.OpenGl;
+
+namespace Tesla.GFX.ModelLoading
+{
+    public class Material
+    {
+        public enum IllumType
+        {
+            FLAT,
+            SPECULAR
+        }
+
+        private float[] ambient, diffuse, specular;
+        private float alpha;
+        private float shininess;
+        private Texture texture;
+        private IllumType illumType;
+
+        public Material(float[] ambient, float[] diffuse, float[] specular, float alpha, float shininess, IllumType illumType, Texture texture)
+        {
+            this.ambient = ambient;
+            this.diffuse = diffuse;
+            this.specular = specular;
+            this.alpha = alpha;
+            this.shininess = shininess;
+            this.illumType = illumType;
+            this.texture = texture;
+        }
+
+        public void SetMaterial()
+        {
+            if(illumType != IllumType.FLAT && illumType == IllumType.SPECULAR)
+                Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, specular);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, ambient);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, diffuse);
+            Gl.glMateriali(Gl.GL_FRONT, Gl.GL_SHININESS, (int)shininess);
+
+            if (texture != null)
+            {
+                texture.Bind();
+                Gl.glEnable(Gl.GL_TEXTURE_2D);
+            }
+        }
+    }
+}
