6bbf41edbede7e1b622aca15b250aeda3a2c9aba
Tobias.Arrskog <Tobias.Arrskog@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2008-12-06 21:21:47 +0000

    * Check.cs: Added a testing utility, we really should have nunit   though...

---
+++ Tesla.Common/Check.cs
@@ -0,0 +1,57 @@
+// Check.cs created with MonoDevelop
+// User: topfs at 9:45 PM 12/6/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace Tesla.Common
+{
+	
+	
+	public class Check
+	{
+		static int succeded = 0;
+		static int failed = 0;
+		public Check()
+		{
+		}
+		
+		public static void AssertEquals(object o1, object o2)
+		{
+			AssertEquals("", o1, o2);
+		}
+	
+		public static void AssertEquals(String s, object o1, object o2)
+		{
+			if (o1.Equals(o2))
+			{
+				s = "Sucess " + (s.Length > 0 ? " - " : "") + s;
+				succeded++;
+			}
+			else
+			{
+				s = "Failed " + (s.Length > 0 ? " - " : "") + s + " " + o1.ToString() + " != " + o2.ToString();
+				failed++;
+			}
+				
+			System.Console.Out.WriteLine(s);
+		}
+		
+		public static int getSucceded()
+		{
+			return succeded;
+		}
+		
+		public static int getFailed()
+		{
+			return failed;
+		}
+		
+		public static void reset()
+		{
+			succeded = failed = 0;
+		}
+	}
+}
