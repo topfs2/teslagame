a507a6ae95c55fff856bf0b9400e725611ec35fb
Tobias.Arrskog <Tobias.Arrskog@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2009-01-28 20:45:48 +0000

    Forgot to add the Texture Interface ;)

---
+++ trunk/Tesla.GFX/Texture/Texture.cs
@@ -0,0 +1,17 @@
+// Texture.cs created with MonoDevelop
+// User: topfs at 6:42 PM 1/28/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace Tesla.GFX
+{
+	
+	
+	public interface Texture
+	{
+		void Bind();
+	}
+}
