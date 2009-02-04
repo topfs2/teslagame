864fcd301ad583d164fc5efaafef44ba7c4291b7
Tobias.Arrskog <Tobias.Arrskog@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2009-02-04 15:59:29 +0000

    * FPSCounterTest.cs: Added a FPSCounter for easier management of   frametime and FPS calculations. Also added an empty starting point   for the new game

---
+++ Tesla/Game.cs
@@ -0,0 +1,66 @@
+// Main.cs created with MonoDevelop
+// User: topfs at 10:10 PM 9/27/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+using System;
+using System.Collections;
+using Tesla.GFX;
+using Tesla.GFX.Font;
+using Tesla.Physics;
+using Tesla.Common;
+using Tesla.GFX.ModelLoading;
+using Tesla.Audio;
+using Tao.Sdl;
+
+namespace Tesla
+{
+	class MainClass
+	{
+		static SDLWindow w;
+        static Configuration c = new Configuration("config.dat");
+		
+		static AudioContext audioContext;
+		
+		static void Initialize()
+		{
+			c  = new Configuration("config.dat");
+			audioContext = new AudioContext(c);
+			
+			w = new SDLWindow(c);
+			w.Add(buttonAction);
+		}
+		
+		static void Deinitialize()
+		{
+			audioContext.Dispose();
+		}
+		
+		static void LoadObjects()
+		{
+			TexturedCube tc = new TexturedCube(new BasicTexture(c.defaultPath + "Texture/Tile/crate.png"), new Vector3f(0.0f, 0.0f, -10.0f), 1.0f, 1.0f, 1.0f);
+			w.Add(tc);
+		}
+		
+		public static void Main(string[] args)
+		{
+			Initialize();
+			
+			LoadObjects();
+			
+			FPSCounter frameCounter = new FPSCounter();
+			bool quitFlag = false;
+			while(!quitFlag)
+			{
+				float frameTime = frameCounter.Update();
+				quitFlag = w.Update(frameTime);
+			}
+
+			Deinitialize();
+		}
+
+		static void buttonAction(byte[] keyState, int numberKeys, float frameTime)
+		{
+		}
+	}
+}
