9d97d065b4f661bce4c0b01ddfcf1257d2643ca5
Tobias.Arrskog <Tobias.Arrskog@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2009-02-06 14:04:17 +0000

    * Tesla/Game.cs: Updated Landscape2D to take translationY (not ideal   and the parameters need to be redone completely to be very usefull)

--- trunk/Tesla/Game.cs
+++ trunk/Tesla/Game.cs
@@ -22,11 +22,12 @@ namespace Tesla
         static Configuration c = new Configuration("config.dat");
 		
 		static AudioContext audioContext;
-		
+		static AudioListener listener; 
 		static void Initialize()
 		{
 			c  = new Configuration("config.dat");
 			audioContext = new AudioContext(c);
+			listener = new AudioListener();
 			
 			w = new SDLWindow(c);
 			w.Add(buttonAction);
@@ -43,10 +44,17 @@ namespace Tesla
 			
 			Font font = SDLFontv2.Create(c.defaultPath + "Fonts/zektonbi.ttf", 32);
 			w.Add(new SimpleFontHandler(font, "welcome to TESLA!", new Vector2f(10.0f, 10.0f), new Color4f(1.0f, 1.0f, 1.0f, 1.0f)));
-			Landscape2D l = new Landscape2D(new BasicTexture(c.defaultPath + "Texture/Tile/rock512.bmp"), new BasicTexture(c.defaultPath + "Texture/Tile/grass.jpg"));
+			Landscape2D l = new Landscape2D(new BasicTexture(c.defaultPath + "Texture/Tile/rock512.bmp"), new BasicTexture(c.defaultPath + "Texture/Tile/grass.jpg"), 0.0f);
 			w.Add(l);
 			
+			Landscape2D l2 = new Landscape2D(new BasicTexture(c.defaultPath + "Texture/Tile/rock512.bmp"), new BasicTexture(c.defaultPath + "Texture/Tile/grass.jpg"), -10.0f);
+			w.Add(l2);	
 			
+			w.Add(new Quad(new BasicTexture(c.defaultPath + "Texture/Foilage/Vine with alpha.png"), new Vector3f(5,-4,0.001f), 8.0f, 1.0f, 1.0f));
+		}
+		
+		static void LoadAudio()
+		{
 		}
 		
 		
@@ -55,6 +63,7 @@ namespace Tesla
 			Initialize();
 			
 			LoadObjects();
+			LoadAudio();
 			
 			w.getActiveCamera().getPosition().set(10.0f, 2.0f, 10.0f);
 			w.getActiveCamera().rotateX(-3.0f);
@@ -66,9 +75,17 @@ namespace Tesla
 			{
 				float frameTime = frameCounter.Update();
 				quitFlag = w.Update(frameTime);
+				updateAudio();
 			}
 
 			Deinitialize();
+		}
+		
+		public static void updateAudio()
+		{
+			Camera c = w.getActiveCamera();
+			listener.setPosition(c.getPosition());
+			listener.setOrientation(c.getFrontVector(), c.getUpVector());
 		}
 
 		static void buttonAction(byte[] keyState, int numberKeys, float frameTime)
