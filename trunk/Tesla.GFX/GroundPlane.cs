8512b9a764f434a963838830841c96fbde66391e
Tobias.Arrskog <Tobias.Arrskog@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2009-03-25 17:59:01 +0000

    * Tesla.GFX/GroundPlane.cs: Removed the landscape as start in game,   using a plane as ground for now

--- trunk/Tesla.GFX/GroundPlane.cs
+++ trunk/Tesla.GFX/GroundPlane.cs
@@ -19,18 +19,24 @@ namespace Tesla.GFX
 		Vector3f pointA, pointB, pointC, pointD;
 		Vector3f normal;
 		Texture texture;
-		int tiles;
+		int tileV, tileU;
 		
-		public GroundPlane(Texture texture, int tiles, Vector3f pointA, float distance) : this(texture, tiles, pointA + new Vector3f(distance / 2.0f, 0.0f, distance / 2.0f), pointA + new Vector3f(distance / 2.0f, 0.0f, -distance / 2.0f), pointA + new Vector3f(-distance / 2.0f, 0.0f, -distance / 2.0f), pointA + new Vector3f(-distance / 2.0f, 0.0f, distance / 2.0f))
+		public GroundPlane(Texture texture, int tileU, int tileV, Vector3f pointA, float distance) : this(texture, tileU, tileV, pointA + new Vector3f(distance / 2.0f, 0.0f, distance / 2.0f), pointA + new Vector3f(distance / 2.0f, 0.0f, -distance / 2.0f), pointA + new Vector3f(-distance / 2.0f, 0.0f, -distance / 2.0f), pointA + new Vector3f(-distance / 2.0f, 0.0f, distance / 2.0f))
 		{
 		
 		}
 		
-		public GroundPlane(Texture texture, int tiles, Vector3f pointA, Vector3f pointB, Vector3f pointC, Vector3f pointD)
+		public GroundPlane(Texture texture, int tileU, int tileV, Vector3f pointA, float width, float depth) : this(texture, tileU, tileV, pointA + new Vector3f(width / 2.0f, 0.0f, depth / 2.0f), pointA + new Vector3f(width / 2.0f, 0.0f, -depth / 2.0f), pointA + new Vector3f(-width / 2.0f, 0.0f, -depth / 2.0f), pointA + new Vector3f(-width / 2.0f, 0.0f, depth / 2.0f))
+		{
+		
+		}
+		
+		public GroundPlane(Texture texture, int tileU, int tileV, Vector3f pointA, Vector3f pointB, Vector3f pointC, Vector3f pointD)
 		{
 			this.texture = texture;
 		
-			this.tiles = tiles;
+			this.tileU = tileU;
+			this.tileV = tileV;
 		
 			this.pointA = pointA;
 			this.pointB = pointB;
@@ -53,9 +59,9 @@ namespace Tesla.GFX
 			Gl.glBegin(Gl.GL_QUADS);	
 			Gl.glNormal3fv(normal.vector);
 			Gl.glTexCoord2f(0	 , 0);		Gl.glVertex3fv(pointA.vector);
-			Gl.glTexCoord2f(0	 , tiles);	Gl.glVertex3fv(pointB.vector);
-			Gl.glTexCoord2f(tiles, tiles); 	Gl.glVertex3fv(pointC.vector);
-			Gl.glTexCoord2f(tiles, 0); 		Gl.glVertex3fv(pointD.vector);
+			Gl.glTexCoord2f(0	 , tileV);	Gl.glVertex3fv(pointB.vector);
+			Gl.glTexCoord2f(tileU, tileV); 	Gl.glVertex3fv(pointC.vector);
+			Gl.glTexCoord2f(tileU, 0); 		Gl.glVertex3fv(pointD.vector);
 			texture.UnBind();
 			Gl.glEnd();
 		}
