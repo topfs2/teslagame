b06ef028ec3fd78d8d461975d4e5564fe1c56967
Tobias.Arrskog <Tobias.Arrskog@2989100a-bb11-11dd-9d36-1d0c09c95ed7>
2009-03-25 15:59:01 +0000

    * Font/NeheFont.cs: Removed NeheFont and altered SDLFont to handle   blendfunc instead of SDLWindow. Also added a Quad2D which could be   used in the upcomming GUI

---
+++ trunk/Tesla.GFX/Quad2D.cs
@@ -0,0 +1,39 @@
+// Quad2D.cs created with MonoDevelop
+// User: topfs at 4:42 PM 2/8/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using Tao.OpenGl;
+
+namespace Tesla.GFX
+{
+	
+	
+	public class Quad2D : Drawable2D
+	{
+		Texture texture;
+		float x, y;
+		
+		public Quad2D(Texture texture, float x, float y)
+		{
+			this.texture = texture;
+			this.x = x;
+			this.y = y;
+		}
+
+		public void Draw (float frameTime)
+		{
+			//texture.Bind();
+            Gl.glTranslatef(x, y, 0.0f);
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glTexCoord2f(0, 0); Gl.glVertex3f(0.0f, 0.0f, 0);
+            Gl.glTexCoord2f(1, 0); Gl.glVertex3f(0.0f + texture.Width(), 0.0f, 0);
+            Gl.glTexCoord2f(1, -1); Gl.glVertex3f(0.0f + texture.Width(), 0.0f + texture.Height(), 0);
+            Gl.glTexCoord2f(0, -1); Gl.glVertex3f(0.0f, 0.0f + texture.Height(), 0);
+            Gl.glEnd();
+            Gl.glTranslatef(-x, -y, 0.0f);
+		}
+	}
+}
